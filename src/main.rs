771e3e2ac7640b3e3ecffaa9df25d4d2a7374741
Daniil <dpancov64@gmail.com>
2022-07-31 20:49:32 +0300

    implemented jumping

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,46 @@ mod camera;
 pub use camera::*;
 
 struct Player;
+struct Jumper {
+    jump_impulse: f32,
+    is_jumping: bool,
+}
+
+fn player_jumps(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut players: Query<(&Jumper, &mut RigidBodyVelocity), With<Player>>,
+) {
+    for (jumper, mut velocity) in players.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Up)
+            || keyboard_input.pressed(KeyCode::Space) && !jumper.is_jumping
+        {
+            velocity.linvel = Vec2::new(0., jumper.jump_impulse).into();
+        }
+    }
+}
+
+fn set_jumping_false_if_touching_floor(
+    entity: Entity,
+    jumper: &mut Jumper,
+    contact_event: &ContactEvent,
+) {
+    if let ContactEvent::Started(h1, h2) = contact_event {
+        if h1.entity() == entity || h2.entity() == entity {
+            jumper.is_jumping = false
+        }
+    }
+}
+
+fn jump_reset(
+    mut query: Query<(Entity, &mut Jumper)>,
+    mut contact_events: EventReader<ContactEvent>,
+) {
+    for contact_event in contact_events.iter() {
+        for (entity, mut jumper) in query.iter_mut() {
+            set_jumping_false_if_touching_floor(entity, &mut jumper, contact_event);
+        }
+    }
+}
 
 fn spawn_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     let rigid_body = RigidBodyBundle {
@@ -33,6 +73,10 @@ fn spawn_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMateri
         })
         .insert_bundle(rigid_body)
         .insert_bundle(collider)
+        .insert(Jumper {
+            jump_impulse: 3.,
+            is_jumping: false,
+        })
         .insert(RigidBodyPositionSync::Discrete)
         .insert(Player);
 }
@@ -77,6 +121,8 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_startup_system(setup.system())
         .add_startup_stage("player_setup", SystemStage::single(spawn_player.system()))
+        .add_system(player_jumps.system())
+        .add_system(jump_reset.system())
         .add_startup_stage("floor_setup", SystemStage::single(spawn_floor.system()))
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(DefaultPlugins)
